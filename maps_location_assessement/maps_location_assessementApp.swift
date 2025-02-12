1580d6dd72cda0a492d2f83465aae46d9698716b
David Onoh <davidonoh@gmail.com>
2025-02-12 17:04:40 +0100

    Initial Commit

---
+++ maps_location_assessement/maps_location_assessementApp.swift
@@ -0,0 +1,17 @@
+//
+//  maps_location_assessementApp.swift
+//  maps_location_assessement
+//
+//  Created by DavidOnoh on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct maps_location_assessementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maps_location_assessementTests/maps_location_assessementTests.swift
@@ -0,0 +1,17 @@
+//
+//  maps_location_assessementTests.swift
+//  maps_location_assessementTests
+//
+//  Created by DavidOnoh on 2/12/25.
+//
+
+import Testing
+@testable import maps_location_assessement
+
+struct maps_location_assessementTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
