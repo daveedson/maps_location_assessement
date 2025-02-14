2b9553570e2792b20e7e70202d808ad1e100cb8b
David Onoh <davidonoh@gmail.com>
2025-02-14 12:31:20 +0100

    added location details view and viewmodel

---
+++ maps_location_assessement/Features/Location/Presentation/View/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  maps_location_assessement
+//
+//  Created by DavidOnoh on 2/13/25.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
---
+++ maps_location_assessement/Features/Location/Presentation/ViewModel/locationDetailViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  locationDetailViewModel.swift
+//  maps_location_assessement
+//
+//  Created by DavidOnoh on 2/14/25.
+//
+
+import Foundation
